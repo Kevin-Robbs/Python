9fc15529cb7c8848b2c9ba880beac34505617c69
Kevin Robbs <robbsk21090@hotmail.com>
2022-08-23 02:08:33 -0400

    Addition of template engine assignment using Flask

--- Flask/Fundamentals/hello_flask/server.py
+++ Flask/Fundamentals/hello_flask/server.py
@@ -1,27 +1,24 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from ctypes.wintypes import INT
+from flask import *  # Import Flask to allow us to create our app
 
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
+@app.route('/play')          # The "@" decorator associates this route with the function immediately following
 
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+def return_index():
+    return render_template('index.html')
 
-@app.route('/dojo')
-    
-def hello_dojo():
-    return 'Dojo'
+@app.route('/play/<num_divs>')          # The "@" decorator associates this route with the function immediately following
 
-@app.route('/say/<name>')
-    
-def hello_user(name):
-    return 'Hello '+ name
+def return_play(num_divs):
+    num_divs = int(num_divs)
+    return render_template('div_game.html', times = num_divs)
 
-@app.route('/repeat/<multiplier>/<word>')
-    
-def multiply_string(multiplier, word):
-    phrase = (word + '\n') * int(multiplier)
-    return phrase
+@app.route('/play/<num_divs>/<div_color>')
+
+def return_color(num_divs, div_color):
+    num_divs = int(num_divs)
+    return render_template('div_game.html', times = num_divs, my_color = div_color)
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
